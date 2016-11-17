93a7990fe721eb0cbb2501eb2b6cad669b62705e
James Morris <me@james-morris.com>
2016-11-17 14:05:04 -0600

    test of github width
    test

---
+++ test.R
@@ -0,0 +1,12 @@
+    fitControl <-
+        trainControl(
+          method = if(exists(traincontrol.method)) preprocess.transforms else formals(trainControl)$method,
+          number = if(exists(traincontrol.number)) preprocess.transforms else formals(trainControl)$number,
+          repeats = if(exists(traincontrol.repeats)) preprocess.transforms else formals(trainControl)$repeats,
+          p = if(exists(traincontrol.p)) preprocess.transforms else formals(trainControl)$p,
+          search = if(exists(traincontrol.search)) preprocess.transforms else formals(trainControl)$search,
+          returnData = if(exists(traincontrol.returnData)) preprocess.transforms else formals(trainControl)$returnData,extra long line for test
+          method = if(exists(traincontrol.method)) preprocess.transforms else formals(trainControl)$method,
+          method = if(exists(traincontrol.method)) preprocess.transforms else formals(trainControl)$method,
+          method = if(exists(traincontrol.method)) preprocess.transforms else formals(trainControl)$method,
+
