5f2129c42f653c5793207569353a4888778b1c3b
James Morris <me@james-morris.com>
2018-02-01 22:36:04 -0500

    fixing suspend.sh to restart if plugged in

--- linuxfiles/opt/suspend.sh
+++ linuxfiles/opt/suspend.sh
@@ -3,4 +3,12 @@
 ONAC=`cat /sys/class/power_supply/AC/online`
 if [[ "ONAC" -eq 0 ]]; then
     systemctl suspend
+else
+    # xautolock -reset
+    # to get around xautolock bug
+    pkill xautolock
+    xautolock -detectsleep \
+        -time 10 -locker "i3lock -nc 000000" \
+        -killtime 20 -killer "/opt/suspend.sh" \
+        -corners ---+ &
 fi
