1f6bcdc1bdb9f9d39e1f5800cf4dd1975ceddd57
James <me@james-morris.com>
2019-06-09 04:31:59 -0400

    add polybar info

---
+++ linuxfiles/home/jmorris/config/polybar/i3wmthemer_bar_launch.sh
@@ -0,0 +1,15 @@
+#!/bin/env sh
+
+pkill polybar
+
+sleep 1;
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload i3wmthemer_bar &
+  done
+else
+  polybar --reload i3wmthemer_bar &
+fi
+
+#polybar i3wmthemer_bar &
