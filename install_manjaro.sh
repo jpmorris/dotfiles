b20bb2f6abc7eed84b902dc7349ee25dc7ec17f2
James Morris <me@james-morris.com>
2020-01-15 23:21:30 -0500

    major changes to install script

--- install_manjaro.sh
+++ install_manjaro.sh
@@ -1,8 +1,8 @@
 # TO RUN:
 # download install_manjaro.sh from github and run on newly installed manjaro machine
 
-# also manually:
-# add 'button.lid_init_state=open' to /etc/default/grub and run update-grub
+# ref: 'razer blade 15 arch' documenation
+
 
 echo "what is the bitlocker recovery password?"
 read bitlockerkey
@@ -11,7 +11,10 @@ sudo pacman -S yay
 
 yay --noconfirm -Syyuu
 
-yay --noconfirm -S google-chrome arandr neovim vlc numlockx thunderbird freerdp remmina xfce4-notes-plugin teamviewer meld polychromatic openrazer-meta redshift firefox spotify flameshot xclip veracrypt synology-cloud-station-drive slack-desktop anydesk
+yay --noconfirm -S google-chrome arandr neovim vlc numlockx thunderbird freerdp remmina xfce4-notes-plugin teamviewer meld polychromatic openrazer-meta redshift firefox spotify flameshot xclip veracrypt synology-cloud-station-drive slack-desktop anydesk skypeforlinux zoom python-notify2
+
+# touchpad
+yay --noconfirm -S xf86-input-libinput
 
 # polybar
 yay --noconfirm -S polybar siji nitrogen rofi weather-icons
@@ -21,11 +24,24 @@ yay --noconfirm -S virtualbox
 
 # video-nvidia
 sudo mhwd -i pci video-hybrid-intel-nvidia-430xx-bumblebee
-ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/screenlayout/default.sh ~/.screenlayout/default.sh
+echo "~/commonDrive/dotfiles/linuxfiles/home/jmorris/screenlayout/default.sh" >> ~/.xprofile
+
+# vim
+yay --noconfirm -S python-pip
+yay --noconfirm -S python2-pip
+sudo pip install neovim
+sudo pip2 install neovim
 
 # audio
 ln -s ~/commonDrive/dotfiles/linuxfiles/etc/systemd/system/resume\@jmorris.service /etc/systemd/system/resume\@jmorris.service
 ln -s ~/commonDrive/dotfiles/linuxfiles/etc/systemd/system/suspend\@jmorris.service /etc/systemd/system/suspend\@jmorris.service
+yay --noconfirm -S pavucontrol
+
+# bluetooth
+yay --noconfirm -S bluez-utils blueman pluseaudio-bluetooth
+
+# power
+yay --noconfirm -S gnome-power-statistics
 
 # dotfiles
 yay -S --noconfirm dislocker-git
@@ -49,6 +65,14 @@ ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/vimrc ~/.vimrc
 ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/vim ~/.vim
 ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/ideavimrc ~/.ideavimrc
 ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/vim ~/.config/nvim
+mkdir ~/.config/polybar
+ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/config/polybar/config-edward ~/.config/polybar/config
+ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/config/polybar/openweathermap-simple-silverspring.sh ~/.config/polybar/
+ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/config/polybar/popup-calendar.sh ~/.config/polybar/
+mkdir -p ~/.config/i3
+ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/config/i3/config.edward ~/.config/i3/config
+sudo ln -s ~/commonDrive/dotfiles/linuxfiles/home/jmorris/config/i3/bright /usr/bin/
+sudo chmod +x /usrkk/bin/bright
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 mkdir -p ~/.vimtmp/backup
@@ -56,4 +80,9 @@ mkdir -p ~/.vimtmp/swap
 mkdir -p ~/.vimtmp/undo
 
 sudo ln -s ~/commonDrive/dotfiles/linuxfiles/usr/bin/myredshift /usr/bin/myredshift
+
+# kernel parameter
+#sudo sed -e '/GRUB_CMDLINE_LINUX_DEFAULT/s/"$/ button.lid_init_state=open i915.edp_vswing=2"/' /etc/default/grub -i
+sudo sed -e '/GRUB_CMDLINE_LINUX_DEFAULT/s/"$/ button.lid_init_state=open i915.enable_rc6=0"/' /etc/default/grub -i
+sudo update-grub
 
