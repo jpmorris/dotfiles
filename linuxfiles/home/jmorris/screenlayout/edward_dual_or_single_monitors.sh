dee81addb963497423d6bedb0f52760798c5050f
James Morris <me@james-morris.com>
2020-03-04 10:42:27 -0500

    udev autodetct on edward

--- linuxfiles/home/jmorris/screenlayout/edward_dual_or_single_monitors.sh
+++ linuxfiles/home/jmorris/screenlayout/edward_dual_or_single_monitors.sh
@@ -9,12 +9,14 @@ function ActivateDual {
 }
 function ActivateTriple {
     echo "Switching to TRIPLE MONITORS"
-    xrandr --output eDP-1 --mode 2560x1440 --pos 3840x0 --rotate normal --output DVI-I-2-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-I-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output eDP-1 --mode 2560x1440 --pos 3840x0 --rotate normal --output DVI-I-3-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-I-2-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 }
 
 function ActivateIntelSingle {
     echo "Switching to LAPTOP ONLY - INTEL"
-    xrandr --output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal
+    #xrandr --output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal
+    xrandr --output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-I-3-2 --off --output DVI-I-2-1 --off
+
 }
 
 function ActivateNVIDIASingle {
@@ -26,7 +28,7 @@ function HDMIConnected {
     xrandr | grep "^HDMI-0 connected"
 }
 function DockConnected {
-    xrandr | grep "^DVI-I-1-1 connected"
+    xrandr | grep "^DVI-I-2-1 connected"
 }
 
 function NVIDIADetected {
