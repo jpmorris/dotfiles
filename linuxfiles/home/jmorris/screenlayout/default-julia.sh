e40eff7ef07b6630ad3b581adb9524e48f729e30
James Morris <me@james-morris.com>
2021-05-05 17:42:34 -0400

    lots of changes

--- linuxfiles/home/jmorris/screenlayout/default-julia.sh
+++ linuxfiles/home/jmorris/screenlayout/default-julia.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-2 --off --output DP-4 --off --output DP-2-8 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP-4-8 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3-8 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-2-1 --off --output DP-3-1 --off --output DP-4-1 --off --output DVI-I-1-1 --off --output HDMI-1-1 --off --output DP-1-1 --off
+xrandr --output DP-0.8 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-4 --off --output DP-5 --mode 2560x1440 --pos 5120x0 --rotate normal
