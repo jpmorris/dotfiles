b734dec58949eb8c2d6103360ab4bc5bc12d647f
James Morris <me@james-morris.com>
2023-01-08 14:02:20 -0500

    changes for lock on desktop

--- linuxfiles/home/jmorris/scripts/lock.sh
+++ linuxfiles/home/jmorris/scripts/lock.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
-i3lock --nofork --show-failed-attempts --ignore-empty-password --image='/home/jmorris/dotfiles/linuxfiles/home/jmorris/Pictures/cyberpunk_cityscape.png'
+bgimage=$(cat ~/.config/nitrogen/bg-saved.cfg | grep file | cut -f2 -d "=")
+#i3lock --nofork --show-failed-attempts --ignore-empty-password --image=$bgimage
+# install betterlockscreen from aur
+betterlockscreen -l dim -- --composite
 xset dpms
