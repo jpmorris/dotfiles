8501a582c3c720c2b24f6928dbe59b5efb282336
James Morris <me@james-morris.com>
2023-01-08 12:27:14 -0500

    fixing laptop sleeping

---
+++ linuxfiles/home/jmorris/scripts/lock.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+i3lock --nofork --show-failed-attempts --ignore-empty-password --image='/home/jmorris/dotfiles/linuxfiles/home/jmorris/Pictures/cyberpunk_cityscape.png'
+xset dpms
