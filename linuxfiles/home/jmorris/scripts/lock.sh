a244866fc7b7a5b5d14c721cbc5e300d37ff38a1
James Morris <me@james-morris.com>
2023-01-14 20:32:23 -0500

    more instructions for betterlockscreen

--- linuxfiles/home/jmorris/scripts/lock.sh
+++ linuxfiles/home/jmorris/scripts/lock.sh
@@ -3,5 +3,6 @@ set -e
 bgimage=$(cat ~/.config/nitrogen/bg-saved.cfg | grep file | cut -f2 -d "=")
 #i3lock --nofork --show-failed-attempts --ignore-empty-password --image=$bgimage
 # install betterlockscreen from aur
+# then run betterlockscreen -u <location of lockscreen image> to generate images
 betterlockscreen -l dim -- --composite
 xset dpms
