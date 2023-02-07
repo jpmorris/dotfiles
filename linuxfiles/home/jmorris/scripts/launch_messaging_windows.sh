885aa88e9d2f0ba02ac8aee2a9821e0198985ee2
James Morris <me@james-morris.com>
2023-02-07 17:06:57 -0500

    adding script to keep messenger windows up

---
+++ linuxfiles/home/jmorris/scripts/launch_messaging_windows.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# checks to make sure google WPA messaging apps are always up
+# see i3 config for more information
+# add to cron
+grep -Po 'class="Google-chrome" instance="crx_\K.*(?=")' ~/.config/i3/config | while read -r line; do
+    #echo $line
+    if [[ ! $(xdotool search -classname $line) ]]; then
+        google-chrome-stable --profile-directory=Default --app-id=$line
+    fi
+done
