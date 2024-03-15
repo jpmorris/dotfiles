90de72cc2a5f00fa8bd24d54034795f719103255
James Morris <me@james-morris.com>
2024-03-15 01:32:10 -0400

    fixing lock script

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -4,6 +4,7 @@ bgimage=$(cat ~/.config/nitrogen/bg-saved.cfg | grep file | cut -f2 -d "=")
 #i3lock --nofork --show-failed-attempts --ignore-empty-password --image=$bgimage
 # install betterlockscreen from aur
 # then run betterlockscreen -u <location of lockscreen image> to generate images
-ps aux | grep chrom  | grep 'disable-databases'| awk '{print $2}' | xargs kill -9
+# incognito kill
+ps aux | grep chrom  | grep 'disable-databases'| awk '{print $2}' | xargs -r kill -9
 betterlockscreen -l dim -- --composite
 xset dpms
