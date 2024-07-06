83dc4385bfa4967e6150e5c09e82b592205903f9
James Morris <me@james-morris.com>
2024-07-06 15:03:24 -0400

    updates to laptop messaging windows

--- scripts/launch_messaging_windows.sh
+++ scripts/launch_messaging_windows.sh
@@ -2,6 +2,7 @@
 # checks to make sure google WPA messaging apps are always up
 # see i3 config for more information
 # add to cron
+# use xprop to get class ids
 grep -Po 'class="Google-chrome" instance="crx_\K.*(?=")' ~/.config/i3/config | while read -r line; do
     echo $line
     if [[ ! $(xdotool search -classname $line) ]]; then
