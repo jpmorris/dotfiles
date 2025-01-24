4ddc42ff2c9af3b3a6753f3216b22732b71cb004
James Morris <me@james-morris.com>
2025-01-23 22:28:56 -0500

    updating lin upgrade packages on wake

--- scripts/update_arch_packages.sh
+++ scripts/update_arch_packages.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+sleep 10
 FOUND_BEBOP=$(ping -c 1 192.168.1.25)
 if [ "$FOUND_BEBOP"=="0" ]; then
+    echo 'waiting for network to come up...'
+    sleep 10
     echo 'updating packages'
-    # need visudo to have yay, pacman, makepkg as NOPASSWD
+    # need to run resume systemd script as root, cant get visudo to work
     yay -Su --answerdiff None --answerclean None --noconfirm
     #rtcwake - only if update prevents sleep on lid close
 fi
